0bab661953aff5fc6713a41ded103c42732f37bf
Antonio <antoniolagundzija@gmail.com>
2024-11-19 17:31:09 +0100

    indeksi crud operacije 2

--- Backend/index1.js
+++ Backend/index1.js
@@ -8,10 +8,43 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/film', (request, response) => {
-    return response.send('Popis filmova');
-    });
+let movies = [
+    {
+        "id": 1,
+        "name": "Lord of the Rings",
+        "producer": "J.R.R. Tolkien",
+        "year": "1954",
+        "category": "Fantasy"
+    },
+    {
+        "id": 2,
+        "name": "Dune",
+        "producer": "Villeneuve",
+        "year": "2021",
+        "category": "Sci-Fi"
+    },
+]
 
+
+
+app.get('/getmovie/:id', (request, response) => {
+        let id = request.params.id;
+        let movie = "";
+
+
+
+        movies.forEach(element => {
+            if(element.id ==id){
+                movie= JSON.stringify(element);
+            }            
+        });
+
+
+
+        return response.send('Getting Movie '+movie);
+        });
+
+        
 app.post('/addMovie', (request, response) => {
     //Ocjena, godina, reziser, tip
     
@@ -21,13 +54,38 @@ app.post('/addMovie', (request, response) => {
     const category = data.category;
     const producer = data.producer;
 
-    console.log(data.name);
-    console.log(data.year);
-    console.log(data.category);
-    console.log(data.producer);
-    return response.send("Dodovanje filmova " +name+" "+year+" "+category+" "+producer+"" );
+    let movie={
+        "id": movies.length+1,
+        "name": name,
+        "producer": producer,
+        "year": year,
+        "category": category
+
+    };
+    movies.push(movie);
+
+
+
+   
+    return response.send('Adding movie. New list:' + JSON.stringify(movies));
+        //' +id+' '+name+" "+year+" "+category+" "+producer);
 });
-app.put('/updateMovie', (request, response) => {
+app.put('/updateMovie/:id', (request, response) => {
+    let id = request.params.id;
+
+    const data = request.body;
+    const name = data.name;
+    const year = data.year;
+    const category = data.category;
+    const producer = data.producer;
+
+    
+    return response.send('Updating movie with id:' +id+' Name:'+name+" Year:"+year+" Category:"+category+" Producer:"+producer);
+    
+});
+
+app.put('/updateMovie:id', (request, response) => {
+
    
     const data = request.body;
     const name = data.name;
@@ -43,13 +101,29 @@ app.put('/updateMovie', (request, response) => {
     return response.send('Azuriranje filmova PUT metoda -> Change '+name+" "+year+" "+category+" "+producer+"");
     
 });
-app.delete('/deleteMovie', (request, response) => {
-    return response.send('Brisanje filmova');
-    const data = request.body;
-    console.log('Delete '+data.kontakt);
-    return response.send('Delete '+data.kontakt);
+
+
+app.delete('/deletemovie', (request, response) => {
+
+
+    return response.send('Deleting moive');
 });
 
+app.delete('/deletemovie/:id', (request, response) => {
+        let id = request.params.id;
+        
+        let movie = "";
+
+
+
+        movies.forEach(element => {
+            if(element.id ==id){
+                movies.pop(element); //nije dobro riješenje, pravo riješenje nepoznato
+            }            
+        });
+
+        return response.send('Deleting Movie with ID: '+id+ "New movie list" + JSON.stringify(movies));
+        });
 
 app.listen(1000, () => {
     console.log("Server running on port 1000");
