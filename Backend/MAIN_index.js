fad8d07c3f281c934465409d52aa65cfdda25aff
Antonio-Lag <antoniolagundzija@gmail.com>
2025-01-21 13:04:11 +0100

    FINAL FINAL FINAL

--- Backend/MAIN_index.js
+++ Backend/MAIN_index.js
@@ -6,7 +6,7 @@ const path = require("path");
 const { request } = require("http");
 
 const app = express();
-const port = 3001;
+
 
 app.use(bodyParser.json());
 
@@ -29,14 +29,40 @@ connection.connect(function(err) {
 app.use(express.static(path.join(__dirname, "..", "frontend")));
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "..", "frontend", "")); 
+    res.sendFile(path.join(__dirname, "..", "frontend", "Register.html")); 
 });
 
+
 app.get("/api/movie", (request, response) => {
-    connection.query("SELECT * FROM Movies", (error, results) => {
+    const category = request.query.category;
+    const query = "SELECT * FROM Movies";
+    const queryParams = [];
+
+
+    if (category) {
+        query += " WHERE category = ?";
+        queryParams.push(category);
+    }
+
+
+    connection.query(query, queryParams, (error, results) => {
         if (error) throw error;
         response.send(results);
     });
+
+});
+
+
+
+app.get("/api/categories", (request, response) => {
+
+    connection.query("SELECT DISTINCT category FROM Movies  ORDER BY category ASC", (error, results) => {
+        if (error) throw error;
+        const categories = results.map(row => row.category);
+        response.send(categories);
+
+    });
+
 });
 
 app.get("/api/movie/:id", (request, response) => {
@@ -47,41 +73,80 @@ app.get("/api/movie/:id", (request, response) => {
     });
 });
 
+
 app.get("/api/series", (request, response) => {
-    connection.query("SELECT * FROM Series", (error, results) => { 
+    const category = request.query.category;
+    const query = "SELECT * FROM Series";
+    const queryParams = [];
+
+    if (category) {
+        query += " WHERE category = ?";
+        queryParams.push(category);
+    }
+
+    connection.query(query, queryParams, (error, results) => {
         if (error) throw error;
         response.send(results);
     });
+
 });
+
+
+
+app.get("/api/categoriess", (request, response) => {
+    connection.query("SELECT DISTINCT category FROM Series ORDER BY category ASC", (error, results) => {
+        if (error) throw error;
+        const categories = results.map(row => row.category);
+        response.send(categories);
+    });
+
+});
+
+
 
 app.get("/api/series/:id", (request, response) => {
-  const id = request.params.id;
-  connection.query("SELECT * FROM Series WHERE id = ?", id, (error, results) => {
-      if (error) throw error;
-      response.send(results);
-  });
+const id = request.params.id;
+connection.query("SELECT * FROM Series WHERE id = ?", id, (error, results) => {
+    if (error) throw error;
+    response.send(results);
+});
 });
 
-//select * from movies where category = ?, [search]
-// to napraviti kao padjući meni
 
+app.get("/api/users", (request, response) => {
+    connection.query("SELECT * FROM Users", (error, results) => { 
+        if (error) throw error;
+            response.send(results);
+        });
+});
 
-// maknuti reservations
-app.post("/api/reser", (request, response) => {
+app.delete("/api/delete_user/:id", (request, response) => {
+    const userId = request.params.id;
+    connection.query("DELETE FROM Users WHERE user_id = ?", [userId], (error, results) => {
+           if (error) throw error;
+            if (results.affectedRows === 0) {    
+            return response.status(404).send({ message: "User  not found" });
+            }    
+        response.send({ message: "User  deleted successfully" });    
+    });
+
+});
+ 
+app.post("/api/insert_movie", (request, response) => {
     const data = request.body;
-    const rezervacija = [[data.date, data.id_movie, data.user]];
+    const newmovie = [[data.movie, data.producer, data.year_mov, data.category, data.image, data.video_id, data.description_mov, data.price]];
 
-    connection.query("INSERT INTO Reservation (date_reservation, movie_id, username) VALUES ?", [rezervacija], (error, results) => {
+    connection.query("INSERT INTO Movies (movie, producer, year_mov, category, image, video_id, description_mov, price) VALUES ?", [newmovie], (error, results) => {
         if (error) throw error;
         response.send(results);
     });
 });
 
-app.post("/api/insert_movie", (request, response) => {
+app.post("/api/insert_series", (request, response) => {
     const data = request.body;
-    const newmovie = [[data.movie, data.producer, data.year_mov, data.category, data.image, data.video_id, data.description_mov]];
+    const newmovie = [[data.serie, data.producer, data.year_mov, data.category, data.image, data.video_id, data.description, data.price]];
 
-    connection.query("INSERT INTO Movies (movie, producer, year_mov, category, image, video_id, description_mov) VALUES ?", [newmovie], (error, results) => {
+    connection.query("INSERT INTO Series (serie, producer, year_mov, category, image, video_id, description, price) VALUES ?", [newmovie], (error, results) => {
         if (error) throw error;
         response.send(results);
     });
@@ -105,6 +170,23 @@ app.post("/api/register", (request, response) => {
     });
 });
 
+
+
+
+app.post("/api/login", (request, response) => {
+    const data = request.body;
+    
+        connection.query("SELECT * FROM Users WHERE username = ? AND password = ?", [data.username,data.password], (error, results) => {
+        if (error) throw error;
+
+        if (results.length == 0) {
+            return response.status(400).json({ message: 'An account with that username does not exist' });
+        }
+        return response.status(200).json({ message: 'Login successful', data: { username: data.username } });
+       
+    });
+});
+
 app.get("/api/checkout", (request,response)=> {
     connection.query("SELECT * FROM Checkout", (error, results) => { 
         if (error) throw error;
@@ -112,31 +194,42 @@ app.get("/api/checkout", (request,response)=> {
     });
 });
 
+
 app.post("/api/insert_checkout", (request, response) => {
-    const data=request.body;
-    const checkout=[[data.id]];
+    const data = request.body; 
+    const checkoutId = data.id;
+    const isSeries = data.isSeries; // true if it's a series, false if it's a movie
 
-    connection.query("INSERT INTO Checkout (id) VALUES ?", [checkout], (error, results) => {
-        if (error) throw error;
+    if (checkoutId == null) {
+        return response.status(400).send({ message: 'Invalid ID provided' });
+    }
+
+
+    let values;
+    let query;
+
+    if (isSeries) {
+        // Insert into id_series
+        query = "INSERT INTO Checkout (id_series) VALUES (?)";
+        values = [[checkoutId]];
+    } else {
+        // Insert into id_movies
+        query = "INSERT INTO Checkout (id_movies) VALUES (?)";
+        values = [[checkoutId]];
+    }
+
+    connection.query(query, values, (error, results) => {
+        if (error) {
+            console.error('Error inserting into checkout:', error);
+            return response.status(500).send('Error inserting into checkout');
+        }
         response.send(results);
     });
 });
-
-app.post("/api/user_checkout", (request, response) => {
-    const data=request.body;
-    const checkout=[[data.username]];
-
-    connection.query("INSERT INTO Checkout (username) VALUES ?", [checkout], (error, results) => {
-        if (error) throw error;
-        response.send(results);
-    });
-});
-
-app.delete("/api/delete_checkout")
 
 
 app.delete("/api/empty_checkout", (request,response)=> {
-    connection.query("DELETE FROM Checkout", (error, results) => { 
+    connection.query("TRUNCATE TABLE Checkout", (error, results) => { 
         if (error) throw error;
         response.send(results);
     });
@@ -144,6 +237,6 @@ app.delete("/api/empty_checkout", (request,response)=> {
 
 
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+app.listen(3001, () => {
+    console.log(`Server running on port 3001`);
 });
